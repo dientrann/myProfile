f23d80d1bec34756dd3ec4d432c32f180e1f15ad
dientrann <trandien99it@gmail.>
2023-11-08 23:14:05 +0700

    myCV

--- src/components/leftComponents/AboutMe.jsx
+++ src/components/leftComponents/AboutMe.jsx
@@ -8,16 +8,16 @@ export default function AboutMe() {
                 I am a cheeful, sociable, enthusiastic, and positive person. I have a strong passion for programming. I also enjoy listening to music and playing sportts</p>
 
             <div className='itemInfo'>
-                <PhoneOutlined style={{ fontSize: "30px", color: "white", borderRadius: "50%", color:"rgb(35 56 77"}} rotate={90} /> <p>0964969163</p>
+                <PhoneOutlined style={{ fontSize: "30px", borderRadius: "50%", color:"rgb(35 56 77"}} rotate={90} /> <p>0964969163</p>
             </div>
             <div className='itemInfo'>
-                <MailOutlined style={{ fontSize: "30px", color: "white", borderRadius: "50%", color:"rgb(35 56 77)"}} /> <p>trandien99it@gmail.com</p>
+                <MailOutlined style={{ fontSize: "30px", borderRadius: "50%", color:"rgb(35 56 77)"}} /> <p>trandien99it@gmail.com</p>
             </div>
             <div className='itemInfo'>
-                <HomeOutlined style={{ fontSize: "30px", color: "white", borderRadius: "50%", color:"rgb(35 56 77)" }} /> <p>Tam Binh, Thu Duc, HCM</p>
+                <HomeOutlined style={{ fontSize: "30px", borderRadius: "50%", color:"rgb(35 56 77)" }} /> <p>Tam Binh, Thu Duc, HCM</p>
             </div>
             <div className='itemInfo'>
-                <GithubFilled style={{ fontSize: "30px", color: "white", borderRadius: "50%", color:"rgb(35 56 77)" }} /> <a href="https://github.com/dientrann" target='_blank'>https://github.com/dientrann</a>
+                <GithubFilled style={{ fontSize: "30px", borderRadius: "50%", color:"rgb(35 56 77)" }} /> <a href="https://github.com/dientrann" target='_blank'>https://github.com/dientrann</a>
             </div>
 
         </div>
